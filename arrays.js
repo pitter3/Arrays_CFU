be68599a0c2afed6d9c96663b67a03cc23135460
Patrick Eitel <patrickeitel12@gmail.com>
2023-06-07 18:25:36 -0700

    Add two programs that show my understanding of index positions and arrays, Add comments explaining my thought process

--- arrays.js
+++ arrays.js
@@ -14,3 +14,20 @@ console.log(fourNumbers);
 //Calling the reverse method upon the fourBooleans variable will reverse the order of the array. The expected output is false, false, true, true.
 fourBooleans.reverse();
 console.log(fourBooleans);
+
+
+
+
+//I based this off of the Replit on the Arrays lesson. I have stored numbers to an array containing all numbers 1-10, but will only print to the terminal multiples of three. To do this I will call on index positions 2 5 and 8, the expected output is 3, 6, and 9.
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
+//Is there a way to do the following next 3 lines all together??
+console.log(numbers[2]);
+console.log(numbers[5]);
+console.log(numbers[8]);
+
+
+
+//This one took some doing, but I figure it out. Slice is another method I learned about, and it should allow me to remove parts of an array, completeNonsense.slice(1, 5) slices the array starting(and including index position 1, 2, 3 and 4, it excludes the last index position of 5). Since slice doesn't actually change the original array like some methods do, I need to store completeNonsense.slice(1, 5) to a new variable. This should print out an array excluding index positions 0, 5, and 6
+var completeNonsense = ["Oklahoma", "This", "sentence", "makes", "sense", "Pork Rinds", "Rock Music"];
+var verySensical = completeNonsense.slice(1, 5);
+console.log(verySensical);
