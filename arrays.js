a5aa521e73326054486290edc6516463c6a7c238
Patrick Eitel <patrickeitel12@gmail.com>
2023-06-07 17:58:22 -0700

    Add three methods applied to each array. Add comments explaining what each method is doing.

--- arrays.js
+++ arrays.js
@@ -1,3 +1,16 @@
 var fourStrings = ["Uno", "Dos", "Tres", "Quatro"];
 var fourNumbers = [2, 4, 6, 8];
 var fourBooleans = [true, true, false, false];
+
+//Calling the pop method upon the fourStrings variable will remove the last element from the array. The last element is "Quatro" so that will be removed.
+fourStrings.pop();
+console.log(fourStrings);
+
+//Calling the push method upon the fourNumbers variable will add whatever is contained in the paraetheses (in this case the number 12) to the end of the array. 
+fourNumbers.push(12);
+console.log(fourNumbers);
+
+
+//Calling the reverse method upon the fourBooleans variable will reverse the order of the array. The expected output is false, false, true, true.
+fourBooleans.reverse();
+console.log(fourBooleans);
